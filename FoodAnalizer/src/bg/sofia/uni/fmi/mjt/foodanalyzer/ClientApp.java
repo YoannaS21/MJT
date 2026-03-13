22c0bec6f173f7552eb90dc89bbfb21130e638f7
Yoanna <rositsa20@gmail.com>
2026-03-13 21:38:14 +0200

    Remove folder

--- FoodAnalizer/src/bg/sofia/uni/fmi/mjt/foodanalyzer/ClientApp.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer;
-
-public class ClientApp {
-    static final int PORT = 18081;
-    public static void main(String[] args) {
-        // Here we start the Client console, that creates a new network client
-        new FoodAnalyzerConsole("localhost", PORT).start();
-    }
-}
--- FoodAnalizer/src/bg/sofia/uni/fmi/mjt/foodanalyzer/ServerApp.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.foodanalyzer;
-
-public class ServerApp {
-    static final int PORT = 18081;
-    public static void main(String[] args) {
-        // Here we create the FoodAnalizerServer and we start it
-        new FoodAnalyzerServer(PORT).start();
-    }
-}
